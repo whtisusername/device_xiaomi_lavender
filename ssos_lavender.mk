95d462c5748004db2f90105853f46437228a3569
zaidkhan0997 <kzaid0997@gmail.com>
2021-01-27 09:47:30 +0530

    lavender: ShapeShiftOS_lavender

--- ssos_lavender.mk
+++ ssos_lavender.mk
@@ -17,12 +17,12 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 $(call inherit-product, device/xiaomi/lavender/dalvikheap.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/ssos/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Hazama25, ak940s
+DEVICE_MAINTAINER := zaidkhan0997
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := ssos_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
