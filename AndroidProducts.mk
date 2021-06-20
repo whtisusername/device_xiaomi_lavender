196fbe0ae84495a5edf3b6b7d3a84eb9a87d9844
mukulsharma06175 <83113849+mukulsharma06175@users.noreply.github.com>
2021-06-20 08:49:53 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/fluid_lavender.mk
+    $(LOCAL_DIR)/aicp_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    fluid_lavender-user \
-    fluid_lavender-userdebug \
-    fluid_lavender-eng
+    acip_lavender-user \
+    acip_lavender-userdebug \
+    acip_lavender-eng
