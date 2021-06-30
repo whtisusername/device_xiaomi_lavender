6d5a15ace0fc88b37c314c7b16565727d0f72c44
Brian Jose <brian.josedxb@gmail.com>
2021-06-30 09:19:33 +0400

    Arrow -> Cipher

--- lineage_lavender.mk
+++ lineage_lavender.mk
@@ -16,13 +16,14 @@ $(call inherit-product, device/xiaomi/lavender/device.mk)
 # Inherit dalvik from native
 $(call inherit-product, device/xiaomi/lavender/dalvikheap.mk)
 
-# Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
-DEVICE_MAINTAINER := Hazama25, ak940s
+TARGET_FACE_UNLOCK_SUPPORTED := true
+CIPHER_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_lavender
+PRODUCT_NAME := lineage_lavender
 PRODUCT_DEVICE := lavender
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 7
